f8218e4b5d5bd27f371dc250be85cff6398cb67e
Jackson Chen <sfc9982@googles.plus>
2024-04-04 18:52:01 +0800

    tidy: extract methods

--- main.cpp
+++ main.cpp
@@ -16,26 +16,118 @@
 #pragma comment(lib, "Ws2_32.lib")
 #endif
 
-std::string extract_payload(std::string const &payload, std::string const &parameter) {
-    boost::regex const pattern(parameter + "=([^&]+)");
-    boost::smatch      match;
-    if (regex_search(payload, match, pattern)) {
-        if (match.size() > 1) {
-            return match[1].str();
+std::vector<std::string> extract_payload(std::string const &payload, std::vector<boost::regex> const &regexes) {
+    std::vector<std::string> para_contents;
+    for (auto const &pattern : regexes) {
+        boost::smatch results;
+        if (regex_search(payload, results, pattern)) {
+            if (results.size() > 1) {
+                std::string para_content = results[1].str();
+                para_contents.emplace_back(para_content);
+            }
         }
     }
-    return std::string{};
+
+    return para_contents;
 }
 
-std::vector<std::string> extract_multiple_payloads(std::string const &payload, std::string const &parameters) {
-    std::vector<std::string> para_contents;
-    std::stringstream        ss(parameters);
-    std::string              parameter;
-    while (std::getline(ss, parameter, ',')) {
-        std::string extracted_payload = extract_payload(payload, parameter);
-        para_contents.push_back(extracted_payload);
+int get_packet_count(std::string const &pcap_path) {
+    int                      packet_count = 0;
+    pcpp::IFileReaderDevice *size_reader  = pcpp::IFileReaderDevice::getReader(pcap_path);
+    pcpp::RawPacket          tmp_packet;
+
+    if (size_reader == nullptr) {
+        std::cerr << "Cannot determine reader for file type" << '\n';
+        return -1;
     }
-    return para_contents;
+
+    if (!size_reader->open()) {
+        std::cerr << "Cannot open " + pcap_path + " for reading" << '\n';
+        return -1;
+    }
+
+    while (size_reader->getNextPacket(tmp_packet)) {
+        packet_count++;
+    }
+
+    size_reader->close();
+    delete size_reader;
+
+    return packet_count;
+}
+
+std::vector<boost::regex> get_regexes(std::string const &parameters) {
+    std::vector<boost::regex> pattern_regexes;
+    std::stringstream         para_ss(parameters);
+    if (parameters.find(',') != std::string::npos) {
+        std::string single_parameter;
+        // multiple parameters
+        while (std::getline(para_ss, single_parameter, ',')) {
+            pattern_regexes.emplace_back(single_parameter + "=([^&]+)");
+        }
+    } else {
+        // single parameter
+        pattern_regexes.emplace_back(parameters + "=([^&]+)");
+    }
+
+    return pattern_regexes;
+}
+
+void match_regex_from_reader(bool debug, std::ofstream &fout, std::string const &pcap_path, const int packetCount, std::vector<boost::regex> const &pattern_regexes) {
+    pcpp::RawPacket          raw_packet;
+    pcpp::IFileReaderDevice *reader = pcpp::IFileReaderDevice::getReader(pcap_path);
+    reader->open();
+
+    int         idx = 0;
+    progressbar pb(100);
+    pb.show_bar(debug);
+
+    while (reader->getNextPacket(raw_packet)) {
+        if (debug) {
+            idx++;
+            // update progress bar every 1% of packets,
+            // 100 is ratio which must equal to value in progressbar variable definition above
+            if (idx % (packetCount / 100) == 0) {
+                pb.update();
+            }
+        }
+
+        pcpp::Packet          parsed_packet(&raw_packet);
+        pcpp::TcpLayer const *tcp_layer = parsed_packet.getLayerOfType<pcpp::TcpLayer>();
+
+        if (tcp_layer == nullptr) {
+            continue;
+        }
+
+        uint8_t const *data_ptr = tcp_layer->getData();
+        size_t const   size     = tcp_layer->getDataLen();
+
+        if (data_ptr == nullptr || size == 0) {
+            continue;
+        }
+
+        std::string              payload(reinterpret_cast<std::string::const_pointer>(data_ptr), size);
+        std::vector<std::string> rsa_list = extract_payload(payload, pattern_regexes);
+
+        if (rsa_list.empty()) {
+            continue;
+        }
+
+        // rsa_list.at(0) is always rsa string which length greater than 16
+        // 16 is not a magic number but a thumb rule because content in rsa is a DES output
+        if (rsa_list.at(0).length() > 16) {
+            for (size_t i = 0; i < rsa_list.size(); ++i) {
+                fout << rsa_list[i];
+                if (i != rsa_list.size() - 1) {
+                    fout << ",";
+                }
+            }
+            fout << '\n';
+        }
+    }
+
+    reader->close();
+    delete reader;
 }
 
 int main(int argc, char *argv[]) {
@@ -76,73 +168,17 @@ int main(int argc, char *argv[]) {
 
     std::ofstream     fout(args::get(output_file));
     std::string const pcap_path = (args::get(input_file));
-    std::string       para(args::get(extpara));
+    std::string       parameters(args::get(extpara));
 
-    // TODO: Get total packet count
-    pcpp::RawPacket          tmp_packet;
-    pcpp::IFileReaderDevice *size_reader = pcpp::IFileReaderDevice::getReader(pcap_path);
-    int                      packetCount = 0;
+    int packetCount = get_packet_count(pcap_path);
 
-    if (size_reader == nullptr) {
-        std::cerr << "Cannot determine reader for file type" << '\n';
-        return 1;
+    if (debug) {
+        std::cout << "Total packets: " << packetCount << '\n';
     }
 
-    if (!size_reader->open()) {
-        std::cerr << "Cannot open " + pcap_path + " for reading" << '\n';
-        return 1;
-    }
+    std::vector<boost::regex> pattern_regexes = get_regexes(parameters);
 
-    while (size_reader->getNextPacket(tmp_packet)) {
-        packetCount++;
-    }
-    size_reader->close();
-    std::cout << "Total packets: " << packetCount << '\n';
-
-    pcpp::RawPacket          raw_packet;
-    pcpp::IFileReaderDevice *reader = pcpp::IFileReaderDevice::getReader(pcap_path);
-    reader->open();
-
-    int         idx = 0;
-    progressbar pb(100);
-    pb.show_bar(debug);
-    while (reader->getNextPacket(raw_packet)) {
-        if (debug) {
-            idx++;
-            if (idx % (packetCount / 100) == 0) {
-                pb.update();
-            }
-        }
-
-        pcpp::Packet parsed_packet(&raw_packet);
-        if (pcpp::TcpLayer const *tcp_layer = parsed_packet.getLayerOfType<pcpp::TcpLayer>()) {
-            uint8_t const *data_ptr = tcp_layer->getData();
-            size_t const   size     = tcp_layer->getDataLen();
-            std::string    payload(reinterpret_cast<std::string::const_pointer>(data_ptr), size);
-            if (para.find(',') != std::string::npos) {
-                // multiple parameters
-                std::vector<std::string> rsa_list = extract_multiple_payloads(payload, para);
-                if (rsa_list.at(0).length() > 16) { // rsa_list.at(0) is always rsa
-                    for (size_t i = 0; i < rsa_list.size(); ++i) {
-                        fout << rsa_list[i];
-                        if (i != rsa_list.size() - 1) {
-                            fout << ",";
-                        }
-                    }
-                    fout << '\n';
-                }
-            } else {
-                // single parameter
-                std::string rsa = extract_payload(payload, para);
-                if (rsa.length() > 16) {
-                    fout << rsa << '\n';
-                }
-            }
-        }
-    }
-
-    std::cout << std::flush;
-    reader->close();
+    match_regex_from_reader(debug, fout, pcap_path, packetCount, pattern_regexes);
 
     return 0;
 }
